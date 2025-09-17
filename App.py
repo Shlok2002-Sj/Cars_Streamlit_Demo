87ef038f86b023cfa4b0484b780b93dd1acbca52
Shlok2002-Sj <jagdishshlok@gmail.com>
2025-09-17 13:02:37 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 
 # Load dataset
-df = pd.read_csv("../DataSets/CARS.csv")
+df = pd.read_csv("CARS.csv")
 
 # Streamlit Title
 st.title("Car Horsepower Comparison")
